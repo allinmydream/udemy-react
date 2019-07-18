124d8580981d07d393608ea4121acd9162e3d1a1
happyjade <jade.d.park@gmail.com>
2019-07-18 17:59:59 +0900

    udemy class 7/18

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const App = () => {
     const profileImg = faker.image.avatar();
     return (
         <div className="ui container comments">
-            <CommentDetail author="judy" timeAgo="Today at 3:00PM" content="Good work" profileImg={profileImg} />
-            <CommentDetail author="noah" timeAgo="Today at 11:35AM" content="nice try!!" profileImg={profileImg} />
-            <CommentDetail author="sam" timeAgo="Yesterday at 10:05PM" content="Can I ask a question?" profileImg={profileImg} />
+            <CommentDetail author="judy" timeAgo="Today at 3:00PM" content="Good work" profileImg={faker.image.avatar()} />
+            <CommentDetail author="noah" timeAgo="Today at 11:35AM" content="nice try!!" profileImg={faker.image.avatar()} />
+            <CommentDetail author="sam" timeAgo="Yesterday at 10:05PM" content="Can I ask a question?" profileImg={faker.image.avatar()} />
         </div>
     )
 }
